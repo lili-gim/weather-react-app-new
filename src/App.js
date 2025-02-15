6aa2ee47b1a1261edbb282b142f06a32a4377b92
lili-gim <156603091+lili-gim@users.noreply.github.com>
2025-02-15 21:54:52 +0100

    Updated App js ans css and html files.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import Weather from "./Weather";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="Berlin" />
+
+        <footer>
+          This project was coded by Lili Gim and is{" "}
+          <a
+            href="https://github.com/lili-gim/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
