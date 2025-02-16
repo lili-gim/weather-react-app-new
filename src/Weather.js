735f4f87625e5bc5cd1c0b82495343df911bee6f
lili-gim <156603091+lili-gim@users.noreply.github.com>
2025-02-16 23:24:06 +0100

    Corrected typo.

--- src/Weather.js
+++ src/Weather.js
@@ -78,7 +78,7 @@ export default function Weather(props) {
 
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
 
-    axios.get(apiUrl).tnen(handleResponse);
+    axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
   }
